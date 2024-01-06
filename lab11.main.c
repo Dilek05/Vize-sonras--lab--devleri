c507e611632ec5fa166d05c2538ff566ee21eacb
Dilek Ünlü <146916274+Dilek05@users.noreply.github.com>
2024-01-06 21:14:09 +0300

    Add files via upload

---
+++ lab11.main.c
@@ -0,0 +1,108 @@
+
+#include <stdio.h>
+
+/*int main(void){
+    char a[50];
+    int i, kelime=0, sayac=0;
+    
+    printf("bir string giriniz: ");
+    fgets(a, sizeof(a), stdin);
+    
+    for (i=0; a[i]!='\0'; i++)
+    {
+        if (a[i]== 'a')
+            
+            sayac++;
+        else if (a[i] == ' ')
+        {
+            kelime++;
+            printf("%d. kelime %d adet a harfi var \n",kelime,sayac);
+            sayac=0;
+        }
+        
+    }
+    kelime++;
+    printf("%d. kelime %d adet a harfi var \n",kelime,sayac);
+    
+    return 0;
+}*/
+
+
+
+
+/*int main(void){
+    char a[50], b[50], gecici;
+    int i,n;
+    
+    printf("birinci stringi giriniz: ");
+    scanf("%s",a);
+    
+    printf("ikinci stringi giriniz: ");
+    scanf("%s",b);
+    
+    for (n=0; a[n]!= '\0'; n++);
+    
+    printf("%s ",b);
+    
+    for (i=0; i<n/2; i++)
+    {
+        gecici= a[n-i-1];
+        a[n-i-1]=a[i];
+        a[i]=gecici;
+    }
+    printf("%s\n",a);
+    
+    return 0;
+}*/
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
